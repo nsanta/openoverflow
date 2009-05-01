d95cd4f5ec3eb5a790bc07074b75cb0ec8c7b715
nicolas <nicolas@nicolas-desktop.(none)>
2009-05-01 19:21:41 -0300

    README changes, add some dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,8 @@ Rails::Initializer.run do |config|
   config.gem "thoughtbot-factory_girl",
              :lib    => "factory_girl",
              :source => "http://gems.github.com"
+  config.gem 'haml'
+  config.gem 'pg'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
