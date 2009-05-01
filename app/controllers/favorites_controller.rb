fcc2628ceb8aabe1fa716090de52fd59a0392225
nicolas <nicolas@nicolas-desktop.(none)>
2009-05-01 20:40:12 -0300

    add specs for favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,11 +3,17 @@ class FavoritesController < ApplicationController
   def create
     @question = Question.find(params[:id])
     current_user.favorite_questions << @question
+    respond_to do |format|
+      format.js {}
+    end
   end
   
   def destroy
     @favorite = current_user.favorites.find_by_question_id(params[:id])
     @favorite.destroy
+    respond_to do |format|
+      format.js {}
+    end
   end
 
 end
--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -2,9 +2,22 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe FavoritesController do
 
-  #Delete this example and add some real ones
-  it "should use FavoritesController" do
-    controller.should be_an_instance_of(FavoritesController)
+  describe "handling POST 'create'" do
+    before :each do
+      @mock_question = mock_model(Question)
+      Question.should_receive(:find).with('1').and_return(@mock_question)
+      @mock_user = mock_model(User , :favorite_questions => mock('questions'))
+      @mock_user.favorite_questions.should_receive('<<').with(@mock_question)
+      controller.stub!(:current_user => @mock_user)
+      post :create , :id => '1'
+    end
+    it "should be success" do
+      response.should be_success
+    end
+    it "should assign the question" do
+      assigns[:question].should == @mock_question
+    end
+    
   end
 
 end
